d0c50b29ee09e3550559134854e7ebbd0283c91d
Tony Tsai <tonyny58@gmail.com>
2022-03-23 11:48:16 +0800

    123

--- test.py
+++ test.py
@@ -1,8 +1,4 @@
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
-
-options = Options()
-options.set_headless(headless=True)
 
 driver = webdriver.Chrome(executable_path='./chromedriver')
 
