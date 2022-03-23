a4e47c8b32ac4120a1c1bde606cd78eebc0ac19d
Tony Tsai <tonyny58@gmail.com>
2022-03-23 12:12:48 +0800

    ok

--- test.py
+++ test.py
@@ -1,17 +1,21 @@
 from selenium import webdriver
-
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
-print("1")
-opts = Options()
-opts.add_argument('--headless')  #無頭chrome
-opts.add_argument('--disable-gpu')
-print("2")
+s=Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=s)
+driver.maximize_window()
+
+# print("1")
+# opts = Options()
+# opts.add_argument('--headless')  #無頭chrome
+# opts.add_argument('--disable-gpu')
+# print("2")
 
 # print(ChromeDriverManager().install())
 
-driver = webdriver.Chrome(executable_path="/home/tonytonytony111136/.wdm/drivers/chromedriver/linux64/99.0.4844.51/chromedriver",chrome_options=opts)
+# driver = webdriver.Chrome(executable_path="/home/tonytonytony111136/.wdm/drivers/chromedriver/linux64/99.0.4844.51/chromedriver",chrome_options=opts)
 
 print("3")
 driver.get("https://zh-tw.facebook.com/")
